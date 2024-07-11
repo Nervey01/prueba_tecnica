c2ec75ccd2dc91b1e8782689fa37f231ad3b8f39
Nervey Rendon <nervey01@gmail.com>
2024-07-11 18:20:52 -0500

    cargue de prueba tecnica de dados

--- dados.py
+++ dados.py
@@ -2,11 +2,9 @@ import tkinter as tk
 from tkinter import messagebox
 import random
 
-# Función para tirar un dado
 def tirar_dado():
     return random.randint(1, 6)
 
-# Función para mostrar el dibujo de un dado en un label
 def mostrar_dado(numero, label):
     if numero == 1:
         label.config(text="-------\n|     |\n|  *  |\n|     |\n-------")
@@ -21,7 +19,6 @@ def mostrar_dado(numero, label):
     elif numero == 6:
         label.config(text="-------\n|*   *|\n|*   *|\n|*   *|\n-------")
 
-# Función para lanzar los dados
 def lanzar_dados():
     try:
         cantidad = int(entry.get())
@@ -40,21 +37,18 @@ def lanzar_dados():
         label.pack(side=tk.LEFT, padx=10)
         mostrar_dado(resultado, label)
 
-# Crear la ventana principal
+
 ventana = tk.Tk()
 ventana.title("Lanzador de Dados")
 
-# Crear la entrada y el botón
 entry = tk.Entry(ventana)
 entry.pack(pady=10)
-entry.insert(0, "2")  # Valor predeterminado
+entry.insert(0, "2")  
 
 boton = tk.Button(ventana, text="Lanzar Dados", command=lanzar_dados)
 boton.pack(pady=10)
 
-# Frame para mostrar los resultados de los dados
 frame_dados = tk.Frame(ventana)
 frame_dados.pack(pady=10)
 
-# Ejecutar la ventana principal
 ventana.mainloop()
